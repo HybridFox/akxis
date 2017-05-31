aed7888aba084338e2d424cb834996c76bc9c67d
Pieterjan Van Saet <hybridfox2@gmail.com>
2017-05-31 22:59:38 +0200

    For no bore

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,30 @@
 $(function(){
-    setTimeout(boxRollovers, 5000)
+    // setTimeout(boxRollovers, 5000)
+    boxRollovers();
+
+    $('.owl-carousel').owlCarousel({
+        items: 4,
+        center: true,
+        loop: true,
+        margin:20,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:false
+            },
+            600:{
+                items:2,
+                nav:false
+            },
+            1000:{
+                items:4,
+                nav:false
+            }
+        },
+        autoplay:true,
+        autoplayTimeout: 3000,
+    });
 });
 
 var layers = [
@@ -46,7 +71,9 @@ setInterval(function() {
     layers[2][0] = layers[2][0] - ((layers[2][0] - XAngle * .3) / 20);
     layers[2][1] = layers[2][1] - ((layers[2][1] - YAngle * .3) / 20);
 
-    $(".loader__matrix img").css({"transform":"perspective(525px) translateZ(" + Z + "px) rotateX(" + layers[0][0] + "deg) rotateY(" + layers[0][1] + "deg)"});
+    $(".loader__matrix .loader__logo-1").css({"transform":"perspective(525px) translateZ(" + Z + "px) rotateX(" + layers[0][0] * 1.1 + "deg) rotateY(" + layers[0][1] * 2 + "deg)"});
+    $(".loader__matrix .loader__logo-2").css({"transform":"perspective(525px) translateZ(" + Z + "px) rotateX(" + layers[0][0] + "deg) rotateY(" + layers[0][1] + "deg)"});
+    $(".loader__matrix .loader__logo-3").css({"transform":"perspective(525px) translateZ(" + Z + "px) rotateX(" + layers[0][0] + "deg) rotateY(" + layers[0][1] + "deg)"});
     $(".loader__logo-background").css({"transform":"perspective(525px) translateZ(" + Z + "px) rotateX(" + layers[1][0] + "deg) rotateY(" + layers[1][1] + "deg)"});
     $(".loader__extra-background").css({"transform":"perspective(525px) translateZ(" + Z + "px) rotateX(" + layers[2][0] + "deg) rotateY(" + layers[2][1] + "deg)"});
 }, 1000 / 60);
