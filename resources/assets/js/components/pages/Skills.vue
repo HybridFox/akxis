eb54de62cf28110ab1d6a140f962b5da54cca9b7
Pieterjan Van Saet <hybridfox2@gmail.com>
2018-06-09 22:22:19 +0200

    Fixed EDGE bug

--- resources/assets/js/components/pages/Skills.vue
+++ resources/assets/js/components/pages/Skills.vue
@@ -1,18 +1,17 @@
 <template>
-    <div class="projects">
+    <div class="projects skills">
         <div class="container">
             <div class="projects__intro">
                 <div class="projects__image"></div>
                 <p>
-                    I create work I love, and I believe in what I do <span></span>
+                    Myself vastly defined with real 21st century skills. <span></span>
                 </p>
             </div>
             <div class="projects__list">
                 <div v-for="skill in skills" class="projects__item">
                     <router-link :to="{ name: 'skill', params: { slug: skill.slug }}">
-                        <p>
-                            {{ skill.slug }}
-                        </p>
+                        <h3>{{ skill.title }}</h3>
+                        <p>{{ skill.skill }}</p>
                     </router-link>
                 </div>
             </div>
