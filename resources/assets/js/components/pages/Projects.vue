c5a2b72c4c4df7e566c1605ac4a7050637302874
Pieterjan Van Saet <hybridfox2@gmail.com>
2018-06-10 23:11:29 +0200

    Updated images

--- resources/assets/js/components/pages/Projects.vue
+++ resources/assets/js/components/pages/Projects.vue
@@ -10,7 +10,7 @@
             <div class="projects__list">
                 <div v-for="project in projects" class="projects__item">
                     <router-link :to="{ name: 'project', params: { slug: project.slug }}">
-                        <img :src="'/assets/img/work/' + project.slug + '.png'" alt="">
+                        <img :src="'/storage/' + project.preview_image" alt="">
                         <div class="projects__info">
                             <h3>{{ project.title }}</h3>
                             <p>{{ project.technologies }}</p>
